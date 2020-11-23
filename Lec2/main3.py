7a589ba6ee8180bad133ecf2f246545bc09d4610
EvgenyVl <evgeny_vlasov@yahoo.com>
2020-11-23 13:00:18 +0300

    Final Lec2 + task

---
+++ Lec2/main3.py
@@ -0,0 +1,10 @@
+"""
+D1:F solution
+"""
+
+#  "Имя: [имя] , Фамилия: [фамилия] , Возраст: [возраст] . Студент BPS"
+name = input()#"Enter name: ")
+last_name = input()#"Enter lastname: ")
+age = input()#"Enter age: ")
+
+print("Имя:", name, ", Фамилия:", last_name, ", Возраст:", age, ". Студент BPS")
